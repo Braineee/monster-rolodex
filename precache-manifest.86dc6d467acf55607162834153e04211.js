116b09fbdeccb908e5fd12f33b8e5c2add150881
David Daniel Igbigbi <daviddaniel@Davids-MacBook-Pro.local>
2020-02-23 10:58:20 +0100

    Updates

---
+++ precache-manifest.86dc6d467acf55607162834153e04211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818e9f550bc2497922b7441a0f79c979",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "78abd710d0aac31599a8",
+    "url": "/monster-rolodex/static/css/main.5e2c2a09.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78abd710d0aac31599a8",
+    "url": "/monster-rolodex/static/js/main.32dc37e4.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
